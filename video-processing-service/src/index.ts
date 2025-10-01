21f7c9277a3a064d973a215c51f0be38e46ee2e1
JanetZhang128 <janetzhangyi@gmail.com>
2025-10-01 13:54:17 +0200

    upload video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Endpoint to process video
+app.post('/process-video', (req, res) => {
+  //get path of the input video from the request body
+  const inputFilePath = req.body.inputFilePath;
+  // Define the output path for the processed video
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send('Bad Request: Missing input or output file path');
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on('end', () => {
+      res.status(200).send('Video processed successfully');
+    })
+    .on('error', (err) => {
+      console.error(`An Error occured:', ${err.message}`);
+      res.status(500).send(`Internal Server Error: `);
+    })
+    .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(
