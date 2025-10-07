25f43ecbb9f76885454b8f022f1392222dc179d2
JanetZhang128 <janetzhangyi@gmail.com>
2025-10-07 14:02:18 +0200

    add generateUploadUrl cloud function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -3,6 +3,8 @@ import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import {UserRecord} from "firebase-functions/v1/auth";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
@@ -10,6 +12,10 @@ const db = new Firestore({
   databaseId: "user",
 });
 
+const storage = new Storage();
+const rawVideoBucketName = "xyz-1001-raw-videos";
+
+// Create a new user document when a user is created
 export const createUser = functions.auth.user().onCreate((user: UserRecord) => {
   const userInfo = {
     uid: user.uid,
@@ -20,4 +26,30 @@ export const createUser = functions.auth.user().onCreate((user: UserRecord) => {
   db.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authentication
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
